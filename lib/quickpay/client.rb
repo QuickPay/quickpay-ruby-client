a41aa74213bb317c276c99cbc4e7a04dddb44cc7
dinesh <dinesh1042@gmail.com>
2015-06-10 15:39:59 +0530

    support for overriding base_uri for client

--- lib/quickpay/client.rb
+++ lib/quickpay/client.rb
@@ -1,15 +1,23 @@
 
 module QuickPay
   class Client  
-    attr_accessor :credential
+    attr_accessor :options
     
-    def initialize (credential = nil)
-      @credential = credential
+    def initialize(*args)
+      if args.length > 1
+        @options = args.dup
+      else
+        @options = { secret: args[0] }
+      end
     end
     
+    def credential
+      options[:secret]
+    end
+
     [:get, :post, :patch, :put, :delete].each do |method|
       define_method(method) do |*args|
-        Request.new(@credential).request(method, *args)
+        Request.new(@options).request(method, *args)
       end
     end
       
